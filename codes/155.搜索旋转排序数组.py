527e500438f24f074d4b6d6c8b86d26cdf3f5d41
Gavince <1259781128@qq.com>
2022-03-12 14:03:14 +0800

    修改

--- codes/155.搜索旋转排序数组.py
+++ codes/155.搜索旋转排序数组.py
@@ -11,10 +11,12 @@ k（0 <= k < nums.length）上进行了 旋转，使数组变为 [nums[k], nums[
 nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。例如， [0,1,2,4,5,6,7] 在下标
 3 处经旋转后可能变为[4,5,6,7,0,1,2] 。给你旋转后的数组 nums 和一个整数 target ，如果
 nums 中存在这个目标值 target ，则返回它的下标，否则返回-1。
+注意:整数数组 nums 按升序排列，数组中的值互不相同
 
 解题方法：
-二分法
-时间复杂度：O(logn)
+使用二分法,通过寻找旋转中心点,以在部分有序数组中寻找目标值
+
+时间复杂度：O(logN)
 空间复杂度：O(1)
 
 原题链接:https://leetcode-cn.com/problems/search-in-rotated-sorted-array/
@@ -26,20 +28,21 @@ class Solution:
 
         if not nums: return -1
 
-        l, r = 0, len(nums) - 1
-        while l <= r:
-
-            mid = (l + r) // 2
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
             if nums[mid] == target:
                 return mid
+            # 1 2 3 4 5
             if nums[0] <= nums[mid]:
                 if nums[0] <= target < nums[mid]:
-                    r = mid - 1
+                    right = mid - 1
                 else:
-                    l = mid + 1
+                    left = mid + 1
+            # 4 5 1 2 3
             else:
                 if nums[mid] < target <= nums[-1]:
-                    l = mid + 1
+                    left = mid + 1
                 else:
-                    r = mid - 1
+                    right = mid - 1
         return -1
---
+++ codes/205.搜索排序数组.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/3/12 上午11:04
+# @Author  : gavin
+# @FileName: 205.搜索排序数组.py
+# @Software: PyCharm
+# @Blog    ：https://blog.csdn.net/weixin_35154281
+"""
+问题描述：
+    搜索旋转数组。给定一个排序后的数组，包含n个整数，但这个数组已被旋转过很多
+次了，次数不详。请编写代码找出数组中的某个元素，假设数组元素原先是按升序排列的。
+注意: 若有多个相同元素，返回索引值最小的一个。
+
+
+示例：
+输入: arr = [15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14],
+target = 5
+输出: 8（元素5在该数组中的索引）
+
+解题方法：
+二分法
+时间复杂度：O(logN)
+空间复杂度：O(1)
+
+原题链接：https://leetcode-cn.com/problems/search-rotate-array-lcci/
+"""
+
+
+class Solution:
+    def search(self, arr: List[int], target: int) -> int:
+        # 二分法算法下进行部分有序查找
+        if not arr: return -1
+        left, right = 0, len(arr) - 1
+        while left <= right:
+            # 1 1 1 2 1 target:1
+            if arr[left] == target:
+                return left
+            mid = (left + right) // 2
+            # 1 2 2 1 1 target:2
+            if arr[mid] == target:
+                right = mid
+            # 1 2 3 4 5
+            elif arr[0] < arr[mid]:
+                if arr[0] <= target < arr[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            # 4 5 1 2 3
+            elif arr[0] > arr[mid]:
+                if arr[mid] < target <= arr[-1]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            # 1 1 1 1 2 1 target:2
+            else:
+                left += 1
+        return -1
